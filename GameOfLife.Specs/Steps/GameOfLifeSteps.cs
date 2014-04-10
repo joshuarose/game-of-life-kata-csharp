fcd34c0c0ad336a966c00da19f6efc95259896a2
jrose <joshuarose@outlook.com>
2014-04-10 12:34:55 -0400

    Added initial boilerplate starter project

---
+++ GameOfLife.Specs/Steps/GameOfLifeSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace GameOfLife.Specs.Steps
+{
+    // The binding attribute tells specflow that this class contains feature steps
+    [Binding]
+    public class GameOfLifeSteps
+    {
+    }
+}
---
+++ GameOfLife.Tests/ExampleTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GameOfLife.Models;
+using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class ExampleUnitTest
+    {
+        [Test]
+        public void True_Is_Truthy()
+        {
+            //A common unit testing pattern is AAA - Arrange, Act, Assert
+
+            //Arrange - Let's pick out all of the lego pieces we need for our test
+            var example = new ExampleModel();
+
+            //Act - Let's perform the action that we need to test
+            var result = example.ReturnTruth();
+
+            //Assert - Let's write what we expect of our result
+            Assert.IsTrue(true);
+
+            /* We should write the above test, before creating our ExampleModel and
+             * build out our features one at a time, the reason for this becomes apparent
+             * with time, you write a failing test first, because seeing the test fail is
+             * the only way to know for sure if your production code is what caused it to pass
+             */
+        }
+    }
+}
